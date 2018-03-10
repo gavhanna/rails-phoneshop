338522eb5e4d905c67683d961d833db819d986fd
Gav Hanna <gavhanna@gmail.com>
2018-03-10 19:58:06 +0000

    added javascript, working app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,34 +1,43 @@
-// window.onload = function(){
-    
-//   const nameIn = document.getElementById("phone_name");
-//   const submitBtn = document.getElementById("submit_button");
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
+window.onload = (function() {
   
-//   nameIn.addEventListener("keyup", checkContent);
+  const title = document.querySelector(".display-4");
+  title.addEventListener("mouseover", sayHi);
+  title.addEventListener("mouseout", backtoNormal);
+  title.style.transform = "scale(1)";
+  title.style.transition = "all .2s ease-in-out";
   
-//   const inputs = document.querySelectorAll(".form-control");
   
-//   inputs.forEach(i => {
-//     i.addEventListener("keyup", checkContent);
-//   })
+  function sayHi() {
+    title.innerText = "Hello Liam!";
+    zoom(title);
+  }
   
-//   // if (nameIn.value.length === 0) {
-//   //   submitBtn.setAttribute("disabled", "")
-//   // }
+  function backtoNormal() {
+    title.innerText = "Phone Shop";
+    zoom(title);
+  }
   
-//   console.log(inputs)
-  
-//   function checkContent(e) {
-//     submitBtn.setAttribute("disabled", "");
-//     let nonEmptyFields = 0;
-//     inputs.forEach(i => {
-//       if (i.value.length > 0) {
-//         nonEmptyFields++;
-//       }
-//     });
-//     if (nonEmptyFields === 5) {
-//       submitBtn.removeAttribute("disabled");
-//     } else {
-//       submitBtn.setAttribute("disabled", "");
-//     }
-//   }
-// };
+  function zoom(el){
+    if (el.style.transform === "scale(1)"){
+      el.style.transform = "scale(1.3)";
+    } else {
+      el.style.transform = "scale(1)";
+    }
+  }
+})
